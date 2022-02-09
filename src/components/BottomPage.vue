5433b6f70f106da73789f9df78f418cf137915ab
Narresx <92748159+Narresx@users.noreply.github.com>
2022-02-09 15:23:44 +0100

    add bottom page elements

--- src/components/BottomPage.vue
+++ src/components/BottomPage.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    <div class="background-gray text-white">
-      <a href="#"
-        ><button class="button-style text-white">SIGN-UP NOW!</button></a
-      >
+    <div class="d-flex container background-gray">
+      <div class="text-white">
+        <a href="#"
+          ><button class="button-style text-white">SIGN-UP NOW!</button></a
+        >
+      </div>
+      <div class="d-flex">
+        <div>
+          <h2 class="text-blue">FOLLOW US</h2>
+          <figure v-for="card in cards" :key="card.url">
+            <img :src="require(`../assets/img/${card.url}`)" />
+          </figure>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,6 +21,18 @@
 <script>
 export default {
   name: "BottomPage",
+
+  data() {
+    return {
+      cards: [
+        { url: "footer-fabook.png" },
+        { url: "footer-twitter.png" },
+        { url: "footer-youtube.png" },
+        { url: "footer-pinterest.png" },
+        { url: "footer-periscope.png" },
+      ],
+    };
+  },
 };
 </script>
 
@@ -33,5 +55,14 @@ export default {
 
 .text-white {
   color: white;
+}
+
+.text-blue {
+  color: rgb(2, 130, 249);
+}
+
+.container {
+  width: 100%;
+  height: 200px;
 }
 </style>
